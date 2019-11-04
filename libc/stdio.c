64065b52fb24a5767fec9dd8a0d3aa373f57de92
Adam Kopczynski <akadamkopczynski@gmail.com>
2019-11-04 21:19:50 +0100

    Fix compiler errors

--- libc/stdio.c
+++ libc/stdio.c
@@ -19,13 +19,13 @@ int puts(const char* string) {
 
 
 
-static bool print(const char* data, size_t length) {
-	const unsigned char* bytes = (const unsigned char*) data;
-	for (size_t i = 0; i < length; i++)
-		if (putchar(bytes[i]) == EOF)
-			return false;
-	return true;
-}
+// static bool print(const char* data, size_t length) {
+// 	const unsigned char* bytes = (const unsigned char*) data;
+// 	for (size_t i = 0; i < length; i++)
+// 		if (putchar(bytes[i]) == EOF)
+// 			return false;
+// 	return true;
+// }
 
 int printf(const char* restrict format, ...) {
 	va_list parameters;
